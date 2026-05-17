162212250e34a70e345d881a029f8317f1697f54
ElthonJohan <jg171020202004@gmail.com>
2026-05-17 13:29:46 -0500

    fix require

--- database.py
+++ database.py
@@ -44,7 +44,7 @@ else:
         pool_pre_ping=True,
         pool_size=5,
         max_overflow=10,
-        connect_args={"sslmode": "disable"} if "postgres" in DATABASE_URL else {},
+        connect_args={"sslmode": "require"} if "postgres" in DATABASE_URL else {},
     )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
