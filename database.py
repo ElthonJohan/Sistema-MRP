6e1fa0fc40d168f8a47a2c3553b15d65c1036aec
Elthon <135673749+ElthonJohan@users.noreply.github.com>
2026-05-17 11:18:44 -0500

    Migración a postgre

--- database.py
+++ database.py
@@ -24,7 +24,7 @@ engine = create_engine(
     pool_pre_ping=True,
     pool_size=5,
     max_overflow=10,
-    connect_args={"sslmode": "require"}
+    connect_args={"sslmode": "disable"}  # Ajusta según tu configuración de PostgreSQL
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
